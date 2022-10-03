79ccbe3a5ad7324e98cd61c36a0ca1077402e575
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-03 19:49:00 -0300

    02 - realizado o requisito 2

---
+++ src/pages/Carregando.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Carregando extends React.Component {
+  render() {
+    return (
+      <div className="main" data-testid="page-album">
+        <h4>
+          Carregando...
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default Carregando;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      checkLength: true,
+      user: '',
+      loading: false,
+    };
+  }
+
+  fetchUser = async () => {
+    const { history } = this.props;
+    const { user } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: user });
+    history.push('/search');
+  };
+
+  checkLogin = (event) => {
+    const { target } = event;
+    this.setState({ user: target.value });
+    return target.value.length >= Number('3')
+      ? this.setState({ checkLength: false })
+      : this.setState({ checkLength: true });
+  };
+
   render() {
+    const { user, checkLength, loading } = this.state;
     return (
       <div className="main" data-testid="page-login">
-        <h1>teste</h1>
+        <h1>Login</h1>
+        <div className="form__login">
+          <label htmlFor="name">
+            Nome:
+            <input
+              type="text"
+              id="name"
+              data-testid="login-name-input"
+              value={ user }
+              onChange={ this.checkLogin }
+            />
+          </label>
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            id="name"
+            disabled={ checkLength }
+            onClick={ this.fetchUser }
+          >
+            Entrar
+          </button>
+          {loading && <p>Carregando...</p>}
+        </div>
       </div>
     );
   }
 }
+
+Login.propTypes = ({
+  login: PropTypes.string,
+}).isRequired;
 
 export default Login;
