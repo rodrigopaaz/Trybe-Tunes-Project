513e8e1fd463d5ee23b679528c5a6afadecb31e8
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-04 17:45:47 -0300

    06 - requisito 6 concluido

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 
 class Album extends React.Component {
   render() {
+    const { match: { params } } = this.props;
     return (
       <div className="main" data-testid="page-album">
         <Header />
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import { getUser } from '../services/userAPI';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -9,16 +11,27 @@ class Search extends React.Component {
       loading: false,
       user: '',
       checkLength: true,
+      showAlbum: false,
+      album: [],
     };
   }
 
   async componentDidMount() {
-    const { user } = this.state;
+    const { user, album } = this.state;
     const usuario = await this.fetchGetUser();
     this.setState({ user: usuario });
     console.log(user);
     this.setState({ loading: false });
+    album.map((artist) => this.setState((prev) => ({ album: [...prev.album, artist] })));
   }
+
+  /*   findAlbum = () => {
+    const { user, album } = this.state;
+    album.map((artist, index) => {
+      console.log(artist);
+      this.setState((prev) => ({ album: [...prev.album, artist] }));
+    });
+  } */
 
   checkLogin = (event) => {
     const { target } = event;
@@ -28,6 +41,14 @@ class Search extends React.Component {
       : this.setState({ checkLength: true });
   };
 
+  fetchAlbum = async () => {
+    const { user } = this.state;
+    const albuns = await searchAlbumsAPI(user);
+    this.setState({ album: albuns });
+    this.setState({ showAlbum: true });
+    this.setState({ albumName: '' });
+  };
+
   fetchGetUser = async () => {
     this.setState({ loading: true });
     const user = await getUser();
@@ -35,7 +56,8 @@ class Search extends React.Component {
   };
 
   render() {
-    const { loading, user, checkLength } = this.state;
+    const { loading, user, checkLength, showAlbum, albumName, album } = this.state;
+    const { history } = this.props;
     return (
       <div className="main" data-testid="page-search">
         <Header />
@@ -44,18 +66,49 @@ class Search extends React.Component {
           <input
             type="text"
             data-testid="search-artist-input"
+            value={ albumName }
             onChange={ this.checkLogin }
           />
           <button
             type="button"
             data-testid="search-artist-button"
             disabled={ checkLength }
+            onClick={ this.fetchAlbum }
           >
             Pesquisar
           </button>
           <p data-testid="header-user-name">
             { user.name }
           </p>
+          {showAlbum
+          && <p>{`Resultado de álbuns de: ${user}`}</p>}
+        </div>
+        <div>
+          { album.length >= 1
+            ? album.map((element, key) => (
+              <div key={ key }>
+                <p>
+                  Álbum
+                  {' '}
+                  {element.collectionName}
+                </p>
+                <p>
+                  Artista
+                  {' '}
+                  {element.artistName}
+                </p>
+                <Link
+                  data-testid={ `link-to-album-${element.collectionId}` }
+                  to={ `./album/${element.collectionId}` }
+                >
+                  Link
+
+                </Link>
+
+              </div>
+            ))
+            : <p>Nenhum álbum foi encontrado</p>}
+
         </div>
 
       </div>
