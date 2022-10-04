5fb25ba4adad1e9e278c7c0bce17ebb8c7db62ad
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-04 00:13:54 -0300

    04 - requisito 4 concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="main" data-testid="header-component">
         <h1>teste</h1>
+        <nav>
+          <Link
+            to="../search"
+            data-testid="link-to-search"
+          />
+          <Link
+            to="../favorites"
+            data-testid="link-to-favorites"
+          />
+          <Link
+            to="../profile"
+            data-testid="link-to-profile"
+          />
+        </nav>
       </div>
     );
   }
